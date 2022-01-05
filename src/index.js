a95241873053e02e783c14f1a5af22c6c90930e9
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-01-05 22:29:16 +0300

    play bg music

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { Howl } from 'howler'
+import Resources from './Resources'
 
 import './Styles/main.css'
+
+window.addEventListener('load', () => {
+  const backgroundSound = new Howl({
+    src: Resources.BackgroundTaiga,
+    autoplay: true,
+    loop: true,
+    volume: 0.5
+  }).play()
+  return () => null
+})
 
 ReactDOM.render(
   <React.StrictMode>
